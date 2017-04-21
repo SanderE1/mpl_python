16a39717b444aa05f5ccfa3e4adb17d9226ff9ae
Drew Avis <drew.avis@gmail.com>
2017-04-20 20:00:54 -0400

    Adding explosions

--- defender/pygame_defender1.py
+++ defender/pygame_defender1.py
@@ -11,6 +11,12 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+
+def update_score(screen, score):
+    message = "Score:{}".format(score)
+    font = pygame.font.SysFont("Verdana", 30)
+    text = font.render(message, False, BLACK)
+    screen.blit(text, (10,10))
  
 pygame.init()
  
@@ -19,6 +25,8 @@ size = (800, 500)
 screenbottom = size[1]
 screentop = 0
 screen = pygame.display.set_mode(size)
+score = 0
+lives = 3
 
 # set up sprite lists
 all_sprites_list = pygame.sprite.Group()
@@ -29,6 +37,7 @@ pygame.display.set_caption("Defender 1")
 
 # load the sounds:
 laser_sound = pygame.mixer.Sound("laser5.ogg")
+explosion_sound = pygame.mixer.Sound("explosion.wav")
  
 # Loop until the user clicks the close button.
 done = False
@@ -46,12 +55,18 @@ class Ship(pygame.sprite.Sprite):
         self.rect.x = 100
         self.rect.y = 250
         self.updown = 0
-        self.speed = 8        
+        self.speed = 8
+        self.exploding = False
+
+    def explode(self):
+        ship.exploding = True
+        self.image = pygame.image.load("exploding.png").convert()
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("bullet.png").convert()
+        self.image.set_colorkey(WHITE)
         self.image2 = self.image # store copy here
         self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
@@ -71,7 +86,22 @@ class Alien(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = size[0] - 50
         self.rect.y = y
-        self.speed = 10
+        self.speed = 5
+
+    def move(self):
+        move = random.randint(0,10)
+        if move == 9:
+            direction = random.randint(-1,1)
+            self.rect.y += direction * self.speed
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("boomo.png").convert()
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         
  
 ship = Ship()
@@ -99,9 +129,9 @@ while not done:
                     bullets.add(bullet)
                                 
             # if the up or down key is pressed, set the direction of movement
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and not ship.exploding:
                 ship.updown = -1
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN and not ship.exploding:
                 ship.updown = 1            
         if event.type == pygame.KEYUP:
             # when you stop pressing, stop moving!
@@ -126,8 +156,7 @@ while not done:
            
 
     add_alien = random.randint(0,10)
-    if add_alien == 9 and len(aliens) < 6:
-        print "adding alien"
+    if add_alien == 9 and len(aliens) < 4:
         alien_y = random.randint(0,size[1])
         alien = Alien(alien_y)
         aliens.add(alien)
@@ -139,14 +168,28 @@ while not done:
         if alien.rect.x < 0:
             aliens.remove(alien)
             all_sprites_list.remove(alien)
+        alien.move()
 
-    hit_list = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    hit_list = pygame.sprite.groupcollide(aliens, bullets, True, True)
+    for hit in hit_list:
+        explosion_sound.play()
+        score += 1
+        explosion = Explosion(hit.rect.x, hit.rect.y)
+        all_sprites_list.add(explosion)
+
+    hit = pygame.sprite.spritecollideany(ship,aliens)
+    if hit and not ship.exploding:
+        ship.explode()
+        
+    if score % 100 == 0:
+        lives += 1
  
     # --- Screen-clearing code goes here
     screen.fill(WHITE)
  
     # --- Drawing code should go here
     all_sprites_list.draw(screen)
+    update_score(screen,score)
  
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
